c2e673a9f6f5945ec532ffb954be9cb692ef7a47
mhwahdan <moustafawahdan55@gmail.com>
2022-12-03 21:00:25 +0200

    Migrated schema to PostGreSQL

--- database/database.sql
+++ database/database.sql
@@ -1,47 +1,68 @@
-USE master
+CREATE DATABASE tcu;
 
-CREATE DATABASE TCU
+\c tcu
 
-GO
+CREATE TABLE Models
+(
+  ModelId SERIAL,
+  Name VARCHAR(100) NOT NULL,
+  ProductionDate DATE NOT NULL,
+  Mancufacturor VARCHAR(100) NOT NULL,
+  PRIMARY KEY (ModelId)
+);
 
-USE TCU
+CREATE TABLE Vehicles
+(
+  Plate_number VARCHAR(50) NOT NULL,
+  VehicleId SERIAL,
+  ModelId BIGINT NOT NULL,
+  PRIMARY KEY (VehicleId),
+  FOREIGN KEY (ModelId) REFERENCES Models(ModelId),
+  UNIQUE (Plate_number)
+);
 
 CREATE TABLE VehiclesLocations
 (
   Latitude FLOAT NOT NULL,
   Longitude FLOAT NOT NULL,
-  UpdateTimeStamp DATETIME NOT NULL DEFAULT now(),
+  UptimestampStamp timestamp NOT NULL DEFAULT now(),
   VehicleId BIGINT NOT NULL ,
-  FOREIGN KEY (VehicleId) REFERENCES Vehicle(VehicleId),
+  FOREIGN KEY (VehicleId) REFERENCES Vehicles(VehicleId),
   PRIMARY KEY (VehicleId)
 );
-CREATE TABLE DevicesLocations
-(
-  Latitude FLOAT NOT NULL,
-  Longitude FLOAT NOT NULL,
-  UpdateTimeStamp DATETIME NOT NULL DEFAULT now() ,
-  DeviceId BIGINT NOT NULL ,
-  FOREIGN KEY (DeviceId) REFERENCES Devices(DeviceId),
-  PRIMARY KEY (DeviceId)
-);
+
+
+
+
 
 CREATE TABLE Users
 (
   Name VARCHAR(150) NOT NULL check (Name NOT LIKE '%[^A-Z ]%'),
   Adress VARCHAR(500) NULL,
-  RegisterDate DATETIME NOT NULL,
-  UserId BIGINT NOT NULL IDENTITY(1,1),
-  PRIMARY KEY (UserId),
+  RegisterDate timestamp NOT NULL,
+  UserId SERIAL NOT NULL,
+  PRIMARY KEY (UserId)
 );
 
 CREATE TABLE Devices
 (
-  DeviceId SERIAL PRIMARY KEY,
+  DeviceId SERIAL,
   MAC_Adress CHAR(18) NOT NULL,
   Type INT NOT NULL,
   UserId BIGINT NOT NULL,
   PRIMARY KEY (DeviceId),
   FOREIGN KEY (UserId) REFERENCES Users(UserId)
+);
+
+
+CREATE TABLE DevicesLocations
+(
+  Latitude FLOAT NOT NULL,
+  Longitude FLOAT NOT NULL,
+  UptimestampStamp timestamp NOT NULL DEFAULT now() ,
+  DeviceId BIGINT NOT NULL ,
+  FOREIGN KEY (DeviceId) REFERENCES Devices(DeviceId),
+  PRIMARY KEY (DeviceId)
 );
 
 CREATE TABLE LockRequests
@@ -50,23 +71,16 @@ CREATE TABLE LockRequests
   VehicleId BIGINT NOT NULL,
   Status INT NOT NULL,
   PRIMARY KEY (UserId, VehicleId),
-  FOREIGN KEY (UserId) REFERENCES User(UserId),
-  FOREIGN KEY (VehicleId) REFERENCES Vehicle(VehicleId)
+  FOREIGN KEY (UserId) REFERENCES Users(UserId),
+  FOREIGN KEY (VehicleId) REFERENCES Vehicles(VehicleId)
 );
 
-CREATE TABLE Models
-(
-  ModelId SERIAL PRIMARY KEY,
-  Name VARCHAR(100) NOT NULL,
-  ProductionDate DATE NOT NULL,
-  Mancufacturor VARCHAR(100) NOT NULL,
-  PRIMARY KEY (ModelId)
-);
+
 
 
 CREATE TABLE Parts
 (
-  PartId SERIAL PRIMARY KEY,
+  PartId SERIAL,
   Value VARCHAR(100) NOT NULL,
   Price FLOAT NULL CHECK (Price>=0),
   ModelId BIGINT NOT NULL,
@@ -78,34 +92,26 @@ CREATE TABLE Parts
 
 CREATE TABLE Descriptions
 (
-  DescriptionId SERIAL PRIMARY KEY,
+  DescriptionId SERIAL,
   Value VARCHAR(250) NOT NULL,
   PartId BIGINT NOT NULL,
   PRIMARY KEY (DescriptionId),
   FOREIGN KEY (PartId) REFERENCES Parts(PartId)
 );
 
-CREATE TABLE Vehicles
-(
-  Plate_number VARCHAR(50) NOT NULL,
-  VehicleId SERIAL PRIMARY KEY,
-  ModelId BIGINT NOT NULL,
-  PRIMARY KEY (VehicleId),
-  FOREIGN KEY (ModelId) REFERENCES Models(ModelId),
-  UNIQUE (Plate_number)
-);
+
 
 
 CREATE TABLE Alerts
 (
   Severity int NOT NULL CHECK (Severity>=0),
   Status int NOT NULL,
-  AlertId SERIAL PRIMARY KEY,
+  AlertId SERIAL,
   VehicleId BIGINT NOT NULL,
   DescriptionId BIGINT NOT NULL,
   PRIMARY KEY (AlertId),
   FOREIGN KEY (DescriptionId) REFERENCES Descriptions(DescriptionId),
-  FOREIGN KEY (VehicleId) REFERENCES Vehicles(VehicleId),
+  FOREIGN KEY (VehicleId) REFERENCES Vehicles(VehicleId)
 );
 
 CREATE TABLE Users_Vehicles
